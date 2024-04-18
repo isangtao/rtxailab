8e35e1b622277d63c0e477d52a1c2133ae93d36d
Michael Carlos <126122780+isangtao@users.noreply.github.com>
2024-04-18 00:19:59 -0700

    Add files via upload

---
+++ ZZ_flowise_cloudsummit_clean.sh
@@ -0,0 +1,9 @@
+curl -fsSL https://ollama.com/install.sh | sh
+ollama pull stablelm-zephyr
+ollama pull nomic-embed-text
+sudo apt update
+
+
+sudo apt install -y npm
+sudo npm install -g flowise
+npx flowise start --FLOWISE_USERNAME=user --FLOWISE_PASSWORD=1234
