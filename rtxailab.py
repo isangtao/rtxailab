4f38e272f32feb7a4064228c65eb72a186fd669f
Michael Carlos <126122780+isangtao@users.noreply.github.com>
2024-11-19 17:24:35 -0800

    Update rtxailab.py
    ee -> e

--- rtxailab.py
+++ rtxailab.py
@@ -125,7 +125,7 @@ with st.sidebar:
     if mode == 'PDF':
         help = "(ChatPDF) tbd"  # I need to research latest tech. Vector stores and embeddings have changed since the last time I implemented this. Also, is this still required with the current large context windows?
     if mode == 'Voice Cloning':
-        help = "Upload a voice sample (3-10sec 22050hz 16bit wav) and enter some text to generate sentences in that voice. Here are some samples: \n\nHello! My name is John Smith. \n\nLarge language models use probability to construct the words in a seequence. For any given sequence, there is a probability distribution of options for the next word. \n\nThis version of chat employs the stock version of the language model. Chat history is not retained between prompts. This mode was included for prompt experimentation, performance benchmarking and quality comparison. "
+        help = "Upload a voice sample (3-10sec 22050hz 16bit wav) and enter some text to generate sentences in that voice. Here are some samples: \n\nHello! My name is John Smith. \n\nLarge language models use probability to construct the words in a sequence. For any given sequence, there is a probability distribution of options for the next word. \n\nThis version of chat employs the stock version of the language model. Chat history is not retained between prompts. This mode was included for prompt experimentation, performance benchmarking and quality comparison. "
     if mode == 'Documentation':
         help = "This displays the live source code"
 
