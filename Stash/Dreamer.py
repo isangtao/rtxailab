0eab3fdfc4726576c3f288109813818001cccc8c
Isang Tao <126122780+isangtao@users.noreply.github.com>
2025-04-25 09:18:29 -0700

    Update Dreamer.py

--- Stash/Dreamer.py
+++ Stash/Dreamer.py
@@ -20,7 +20,7 @@ with st.sidebar:
 	name = st.text_input("Protagonist's name", "Michael Carlos")
 	ethnicity = st.text_input("Ethnicity", "Filipino")
 	gender = st.text_input("Gender", "Male")
-	age = st.text_input("Age", "45")
+	age = st.text_input("Age", "55")
 	height = st.text_input("Height", "5ft 10in")
 	interests = st.text_input("Interests", "motorcycling, aikido, archery, hiking, kayaking, snorkeling")
 	occupation = st.text_input("Occupation", "AI Researcher")
