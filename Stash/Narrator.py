739b17ff2b939010b13c10c331f35a9628ef0325
Isang Tao <126122780+isangtao@users.noreply.github.com>
2025-04-27 20:22:12 -0700

    Update Narrator.py

--- Stash/Narrator.py
+++ Stash/Narrator.py
@@ -31,12 +31,12 @@ with st.sidebar:
 	gender = st.text_input("Gender", "Male")
 	age = st.text_input("Age", "55")
 	height = st.text_input("Height", "5ft 10in")
-	interests = st.text_input("Interests", "motorcycling, aikido, archery, hiking, kayaking, snorkeling")
+	interests = st.text_input("Interests", "robotics, motorcycling, aikido, archery, hiking, kayaking, snorkeling")
 	occupation = st.text_input("Occupation", "AI Researcher")
 	country = st.text_input("Country", "Canada")
 	city = st.text_input("City", "Vancouver")
-	other = st.text_input("Other", "Founded A G I Labs Inc., a company focused on A G I research")
-	additional = st.text_input("Additional Instructions", "Make it a happy story with sunshine and warm weather. Set it in the forests of British Columbia. Depict Robots as decent, helpful and protective.")
+	other = st.text_input("Other", "Founded AGI Labs Inc, a company focused on AGI research")
+	additional = st.text_input("Additional Instructions", "Don't mention rain or echoes. Make it a happy story with sunshine and warm weather. Set it in the forests of British Columbia. Depict Robots as decent, helpful and protective. Make it a happy story with sunshine and warm weather. Set it in the forests of British Columbia. Depict Robots as decent, helpful and protective.")
 	style = st.selectbox("Style", ("Adult novel", "Teen adventure", "Childrens' book"))
 	plot_type = st.selectbox("Type", ("Science fiction", "Suspense", "Thriller", "Action", "Adventure", "Fantasy", "Horror", "Mystery"))
 	context = f"Details about the protagonist of a story follows. \n\nName:{name}\n\nEthnicity:{ethnicity}\n\nGender:{gender}\n\nAge:{age}\n\nHeight:{height}\n\nInterests:{interests}\n\nOccupation:{occupation}\n\nCountry:{country}\n\nCity:{city}\n\nOther:{other}\n\n"
@@ -45,7 +45,7 @@ if st.button("Generate"):
 	plot_prompt = f"{context}Provide an unusual {plot_type} plot based on the information above. Don't explain or preamble. {additional} Just state the plot summary in one sentence."
 	completion = client.chat.completions.create(model=api_model, messages=[{"role": "user", "content": plot_prompt ,},], temperature=0.95)
 	plot = completion.choices[0].message.content
-	template_combined = f"{context}\n\nPlot:{plot}\n\nIn the style of a(n) {style}, write a story based on the plot. Provide no explanation or preamble. Just state the title and jump into the story.\n\n"
+	template_combined = f"{context}\n\nPlot:{plot}\n\nIn the style of a(n) {style}, write a story based on the plot. {additional} Provide no explanation or preamble. Just state the title and jump into the story.\n\n"
 	with st.expander("Prompt Info"):
 		st.write(template_combined)
 	stream = client.chat.completions.create(model=api_model, messages=[{"role": "user", "content": template_combined,},], temperature=0.95, stream = True)
@@ -58,4 +58,3 @@ if st.button("Generate"):
 				msg.markdown(response)
 		get_tts().tts_to_file(text=response, speaker_wav="sample.wav", language="en", file_path="output.wav")
 		st.audio("output.wav")
-
