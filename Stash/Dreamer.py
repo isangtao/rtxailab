eb4f4d6867c37ab1b99a5207f0ce912ca5996316
Isang Tao <126122780+isangtao@users.noreply.github.com>
2025-05-04 19:51:15 -0700

    Update Dreamer.py

--- Stash/Dreamer.py
+++ Stash/Dreamer.py
@@ -47,4 +47,4 @@ if st.button("Generate"):
 			if chunk.choices[0].delta:
 				response += chunk.choices[0].delta.content
 				msg.markdown(response)
-		st.write("----------\n\nUsing the attached photo(s) of the protagonist, generate 10 images that show the progression of the story above. Do not include text. Do not depict children. If you can't generate an image, skip that part of the story and generate the next image.  https://copilot.microsoft.com/ https://aistudio.google.com/")
+		st.write("----------\n\nUsing the attached photo(s) of the protagonist, generate 10 images that show the key moments of the story above. Do not include text. Do not depict children. If you can't generate an image, skip that part of the story and generate the next image.  https://copilot.microsoft.com/ https://aistudio.google.com/")
